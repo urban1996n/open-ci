6b8aa4850820e33a4a407f7973b15ce8066b2fc8
Wojciech Kluz <wojciechkluz@kramsoft.pl>
2023-01-24 18:24:25 +0100

    Make Job commit-aware, each job will have the context of a commit, so it can be ran respectively

--- src/src/Job/Data/Job.php
+++ src/src/Job/Data/Job.php
@@ -4,14 +4,17 @@ namespace App\Job\Data;
 
 class Job
 {
-    public function __construct()
-    {
-    }
+    private string $currentCommit;
 
     private bool $state = false;
 
-    public function execute(): void
+    public function __construct(string $currentCommit)
     {
-        
+        $this->currentCommit = $currentCommit;
+    }
+
+    public function getCurrentCommit(): string
+    {
+        return $this->currentCommit;
     }
 }
