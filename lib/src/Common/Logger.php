b81d93721b474347d0fd959c3cbe7b50a9c1731d
Wojciech Kluz <wojciechkluz@kramsoft.pl>
2023-01-24 18:57:50 +0100

    Add initial handler configuration for logger

--- lib/src/Common/Logger.php
+++ lib/src/Common/Logger.php
@@ -3,12 +3,18 @@
 namespace App\Common;
 
 use Monolog\Logger as BaseLogger;
+use Monolog\Handler\StreamHandler;
+use Monolog\Level;
 
 class Logger extends BaseLogger
 {
-    public function __construct()
+    private string $rootDir;
+
+    public function __construct(string $rootDir)
     {
         parent::__construct('ci_cd_app');
+
+        $this->pushHandler(new StreamHandler($rootDir . '/../var/' . $this->name . '.log', Level::Info));
     }
 
     public function setName(string $name): void
