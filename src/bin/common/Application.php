1f251eef112c5a15b66ccc880c594f89e6f44ac7
Wojciech Kluz <wojciechkluz@kramsoft.pl>
2023-01-24 18:17:02 +0100

    Add method imitating Kernel::getRootDir to make sure command's context will never change

--- src/bin/common/Application.php
+++ src/bin/common/Application.php
@@ -1,16 +1,22 @@
 <?php
 
-require_once dirname(__DIR__).'/../vendor/autoload_runtime.php';
+require_once \dirname(__DIR__).'/../vendor/autoload_runtime.php';
 
 use Symfony\Component\Console\Application as BaseApplication;
 
 class Application extends BaseApplication
 {
-    public function __construct(iterable $commands = []) {
+    public function __construct(iterable $commands = [])
+    {
         parent::__construct('ci_cd_client', 0.01);
 
         foreach ($commands as $command) {
             $this->add($command);
         }
+    }
+
+    public static function getRootDirectory(): string
+    {
+        return \dirname(__DIR__);
     }
 }
