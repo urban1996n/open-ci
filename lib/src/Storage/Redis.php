b16fa65946e8dadc36d83c827e1434a647d3ce67
Wojciech Kluz <wojciechkluz@kramsoft.pl>
2023-03-27 18:47:40 +0200

    Add redis authentication

--- lib/src/Storage/Redis.php
+++ lib/src/Storage/Redis.php
@@ -6,10 +6,11 @@ use \Redis as BaseRedis;
 
 class Redis extends BaseRedis
 {
-    public function __construct(private readonly string $redisUrl)
+    public function __construct(private readonly string $redisUrl, string $redisPassword)
     {
         parent::__construct();
 
         $this->connect($this->redisUrl);
+        $this->auth($redisPassword);
     }
 }
