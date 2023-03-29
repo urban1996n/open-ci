eb2123391ccf26a9ab476b764e5af90ef3bf0ecc
Wojciech Kluz <wojciechkluz@kramsoft.pl>
2023-03-29 16:23:50 +0200

    Add event listener for amqp errors
    This will allow to store unprocessed git push requests so they can get queued once the rabbitmq server is available again

--- lib/src/Common/Event/AmqpExceptionListener.php
+++ lib/src/Common/Event/AmqpExceptionListener.php
@@ -1,8 +1,48 @@
 <?php
 
-namespace App\Job\Event;
+namespace App\Common\Event;
 
-class AmqpExceptionHandler
+use App\Storage\Redis;
+use PhpAmqpLib\Exception\AMQPIOException;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+
+#[AsEventListener(event: 'kernel.exception', method: 'onAMQPIOException')]
+class AmqpExceptionListener
 {
+    public function __construct(
+        private readonly LoggerInterface $logger,
+        private readonly Redis $redis
+    ) {
+    }
 
+    public function onAMQPIOException(ExceptionEvent $event): void
+    {
+        $exception = $event->getThrowable();
+
+        if (!$exception instanceof AMQPIOException) {
+            return;
+        }
+
+        $this->logger->error($exception->getMessage());
+        $request = $event->getRequest();
+        $commit  = $request->request->get('after');
+        $branch  = \explode('/', $request->request->get('ref'));
+        $branch  = \end($branch);
+
+        if ($commit && $branch) {
+            $undeliveredJobs = $this->redis->get(Redis::REDIS_KEY_UNDELIVERED_TAGS)
+                ? \json_decode($this->redis->get(Redis::REDIS_KEY_UNDELIVERED_TAGS), true)
+                : [];
+
+            $findSimilar = fn(array $jobConfig) => $jobConfig['branch'] === $branch && $jobConfig['commit'] === $commit;
+
+            if (!\array_filter($undeliveredJobs, $findSimilar)) {
+                $undeliveredJobs[] = ['branch' => $branch, 'commit' => $commit];
+            }
+
+            $this->redis->set(Redis::REDIS_KEY_UNDELIVERED_TAGS, \json_encode($undeliveredJobs));
+        }
+    }
 }
