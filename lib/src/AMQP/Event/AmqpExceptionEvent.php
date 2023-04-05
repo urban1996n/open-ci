a57abf71cb2a261f80f63ebdacf5607b3480c26e
Wojciech Kluz <wojciechkluz@kramsoft.pl>
2023-04-05 21:04:53 +0200

    Use more strict typing for exception argument

--- lib/src/AMQP/Event/AmqpExceptionEvent.php
+++ lib/src/AMQP/Event/AmqpExceptionEvent.php
@@ -2,12 +2,13 @@
 
 namespace App\AMQP\Event;
 
+use PhpAmqpLib\Exception\AMQPExceptionInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Contracts\EventDispatcher\Event;
 
 class AmqpExceptionEvent extends Event
 {
-    public function __construct(private readonly Request $request, private readonly \Throwable $exception)
+    public function __construct(private readonly Request $request, private readonly AMQPExceptionInterface $exception)
     {
     }
 
@@ -16,7 +17,7 @@ class AmqpExceptionEvent extends Event
         return $this->request;
     }
 
-    public function getException(): \Throwable
+    public function getException(): AMQPExceptionInterface
     {
         return $this->exception;
     }
