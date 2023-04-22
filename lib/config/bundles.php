816ac02a953b62e4406da6eee8c3475ea3add9cb
Wojciech Kluz <wojciechkluz@kramsoft.pl>
2023-04-22 23:55:05 +0200

    Register sentry bundle

--- lib/config/bundles.php
+++ lib/config/bundles.php
@@ -6,5 +6,6 @@ use Symfony\Bundle\MonologBundle\MonologBundle;
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     MonologBundle::class => ['all' => true],
-    DoctrineBundle::class => ['all' => true]
+    DoctrineBundle::class => ['all' => true],
+    \Sentry\SentryBundle\SentryBundle::class => ['all' => true],
 ];
