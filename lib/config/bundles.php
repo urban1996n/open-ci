c25d28005cf013ce1746f4c223050a7b205fda85
Wojciech Kluz <wkluz@MacBook-Air-Wojciech.local>
2023-06-24 20:55:30 +0200

    Add tewig bundle globally

--- lib/config/bundles.php
+++ lib/config/bundles.php
@@ -1,12 +1,12 @@
 <?php
 
-use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
 use Sentry\SentryBundle\SentryBundle;
 use Symfony\Bundle\MonologBundle\MonologBundle;
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     MonologBundle::class => ['all' => true],
-    DoctrineBundle::class => ['all' => true],
     SentryBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
 ];
