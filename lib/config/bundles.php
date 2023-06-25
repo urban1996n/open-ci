1c3ecd1d3f8174c3b66cc63debb1aeee3ac7820c
Wojciech Kluz <urban1996n@gmail.com>
2023-06-25 19:45:48 +0200

    Add more robust configuration for docker apache

--- lib/config/bundles.php
+++ lib/config/bundles.php
@@ -2,6 +2,7 @@
 
 use Sentry\SentryBundle\SentryBundle;
 use Symfony\Bundle\MonologBundle\MonologBundle;
+use Symfony\WebpackEncoreBundle\WebpackEncoreBundle;
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
@@ -9,4 +10,5 @@ return [
     SentryBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    WebpackEncoreBundle::class => ['all' => true]
 ];
